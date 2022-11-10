d8ccf4b55abc5a72f910696285e0f8f5b273051a
Omolara Idowu <70015447+Swan-ctrl@users.noreply.github.com>
2022-11-10 11:15:37 +0100

    Implement Countries Fragment
    * Create Countries Fragment
    * Create Bottom Sheet

--- app/src/main/java/com/swancodes/countriesoftheworld/MainActivity.kt
+++ app/src/main/java/com/swancodes/countriesoftheworld/MainActivity.kt
@@ -1,11 +1,14 @@
 package com.swancodes.countriesoftheworld
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.swancodes.countriesoftheworld.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
--- app/src/main/java/com/swancodes/countriesoftheworld/fragments/CountriesFragment.kt
+++ app/src/main/java/com/swancodes/countriesoftheworld/fragments/CountriesFragment.kt
@@ -1,11 +1,12 @@
 package com.swancodes.countriesoftheworld.fragments
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.swancodes.countriesoftheworld.R
+import androidx.fragment.app.FragmentActivity
+import com.google.android.material.bottomsheet.BottomSheetDialogFragment
 import com.swancodes.countriesoftheworld.data.network.CountriesApiClient
 import com.swancodes.countriesoftheworld.databinding.FragmentCountriesBinding
 import com.swancodes.countriesoftheworld.model.CountryBaseResponse
@@ -13,7 +14,7 @@ import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
 
-class CountriesFragment : Fragment() {
+class CountriesFragment : BottomSheetDialogFragment() {
 
     private lateinit var binding: FragmentCountriesBinding
 
@@ -29,15 +30,24 @@ class CountriesFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        CountriesApiClient.retrofitService.getCountriesData().enqueue(object : Callback<CountryBaseResponse> {
-            override fun onResponse(call: Call<CountryBaseResponse>, response: Response<CountryBaseResponse>) {
+        binding.languageButton.setOnClickListener {
+            val bottomSheet = BottomFragment()
+            val fragmentManager = (activity as FragmentActivity).supportFragmentManager
+            fragmentManager.let { bottomSheet.show(it, BottomFragment.TAG) }
 
-            }
+            CountriesApiClient.retrofitService.getCountriesData()
+                .enqueue(object : Callback<CountryBaseResponse> {
+                    override fun onResponse(
+                        call: Call<CountryBaseResponse>,
+                        response: Response<CountryBaseResponse>
+                    ) {
 
-            override fun onFailure(call: Call<CountryBaseResponse>, t: Throwable) {
+                    }
 
-            }
-        })
-
+                    override fun onFailure(call: Call<CountryBaseResponse>, t: Throwable) {
+                        Log.d("MainActivity", "onFailure: " + t.message)
+                    }
+                })
+        }
     }
 }
